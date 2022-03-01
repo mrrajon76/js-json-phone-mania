dc81bc1010ceb64d5a22e6ace52c2719ce5c3549
Md. Rasheduzzaman Rajon <mrrajon76@gmail.com>
2022-03-01 10:27:14 +0600

    Fetch results using API

--- js/phone_api.js
+++ js/phone_api.js
@@ -1,1 +1,16 @@
-console.log('Testing..');
+const getResult = searchKeyword => {
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchKeyword}`)
+        .then(response => response.json())
+        .then(values => showResults(values.data))
+};
+
+const showResults = resultDatabase => {
+    for (data of resultDatabase) {
+        console.log(data);
+    }
+};
+
+document.getElementById('searchBtn').addEventListener('click', function () {
+    const searchKeyword = document.getElementById('searchField').value;
+    getResult(searchKeyword);
+});
