fe7792268d7418ae05fe7beb5a6b26da578bdc83
Md. Rasheduzzaman Rajon <mrrajon76@gmail.com>
2022-03-01 10:53:35 +0600

    Show results

--- js/phone_api.js
+++ js/phone_api.js
@@ -5,9 +5,29 @@ const getResult = searchKeyword => {
 };
 
 const showResults = resultDatabase => {
+    const container = document.getElementById('resultContainer');
+    container.textContent = "";
     for (data of resultDatabase) {
-        console.log(data);
+        createResultCard(data);
     }
+};
+
+const createResultCard = value => {
+    const container = document.getElementById('resultContainer');
+    const colDiv = document.createElement('div');
+    colDiv.innerHTML = `
+        <div class="card h-100">
+            <div class="w-50 mx-auto">
+                <img src="${value.image}" class="card-img-top img-fluid" alt="...">
+            </div>
+            <div class="card-body text-center">
+                <h3 class="card-title">${value.phone_name}</h3>
+            </div>
+            <div class="card-footer bg-brand text-center">
+                <h5 class="card-title">Brand: ${value.brand}</h5>
+            </div>
+        </div>`;
+    container.appendChild(colDiv);
 };
 
 document.getElementById('searchBtn').addEventListener('click', function () {
